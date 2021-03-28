8e991974792e5580c607aa0d6b74429aca441cba
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-03-28 14:25:44 +1100

    Copy some setup from the shared proto folder

---
+++ genproto.ps1
@@ -0,0 +1,18 @@
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.25.0
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.0.0
+go get google.golang.org/grpc@v1.34.0
+go install github.com/LLKennedy/mercury/cmd/protoc-gen-mercury@v0.9.0
+go install github.com/LLKennedy/protoc-gen-tsjson@v0.5.0
+
+$Directory = "./api/*"
+$IncludeRule = "*.proto"
+$ExcludeRUle = [Regex]'.*google.*|.*audit/.*|.*node_modules.*'
+$GoPBPath = "./demopb"
+$TSPBPath = "./api/src"
+$ProtoFiles = Get-ChildItem -path $Directory -Include $IncludeRule | Where-Object FullName -NotMatch $ExcludeRUle
+foreach ($file in $ProtoFiles) {
+	protoc --proto_path="$($file.DirectoryName)" --go_out=paths=source_relative:$GoPBPath --go-grpc_out=paths=source_relative:$GoPBPath --tsjson_out=$TSPBPath --mercury_out=$TSPBPath $file.FullName
+}
+
+go build $PBPath
+go mod tidy
